332661f64a4c38310f235a531d4adfc1979c47ca
Nehith Mateti <nehith.m@gmail.com>
2022-05-07 15:15:24 +0530

    checkin by Abdul

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi I am madhu");
-Console.ReadLine();
+Console.WriteLine("Abdul");
+Console.WriteLine("Abdul");
+Console.WriteLine("Abdul");
+Console.WriteLine("Abdul");
+Console.WriteLine("Abdul");
+Console.WriteLine("Abdul");
 
-
-
-
-Console.WriteLine("Hello my name is vinod");
 Console.ReadLine();
