6673e2a662d56ba35dd3a8cecbe12c515759794d
nehithmateti <92224631+nehithmateti@users.noreply.github.com>
2022-05-07 15:46:24 +0530

    Update Program.cs

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
-Console.WriteLine("Hi I am madhu");
 Console.ReadLine();
