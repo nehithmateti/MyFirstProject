555f7056f4866cd3aa8247bde98c45ce13de70c8
nehithmateti <92224631+nehithmateti@users.noreply.github.com>
2022-05-07 15:14:18 +0530

    Updated program.cs
    I have made my changes

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi I am madhu");
-Console.ReadLine();
-
-
-
-
-Console.WriteLine("Hello my name is vinod");
+Console.WriteLine("Hi I am Jsai);
 Console.ReadLine();
