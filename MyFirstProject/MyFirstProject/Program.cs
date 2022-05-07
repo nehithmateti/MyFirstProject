21f3721b3ab30b28d58a6b37112917824924998f
Nehith Mateti <nehith.m@gmail.com>
2022-05-07 15:29:49 +0530

    test

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,11 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi I am Jsai");
-Console.WriteLine("Abdul");
-Console.WriteLine("Abdul");
-Console.WriteLine("Abdul");
-Console.WriteLine("Abdul");
-Console.WriteLine("Abdul");
-Console.WriteLine("Abdul");
-
 Console.ReadLine();
