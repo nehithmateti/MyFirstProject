3337a6538a20b79a28e487707a38f190a671f1f3
Nehith Mateti <nehith.m@gmail.com>
2022-05-07 15:52:31 +0530

    commit

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hi I am Abdul");
+Console.WriteLine("Hi I am Abdul");
+Console.WriteLine("Hi I am Abdul");
+Console.WriteLine("Hi I am Abdul");
+Console.WriteLine("Hi I am Abdul");
+Console.WriteLine("Hi I am Abdul");
 Console.ReadLine();
