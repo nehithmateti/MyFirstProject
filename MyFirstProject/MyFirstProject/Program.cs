c283ef9cf82f9c2a4d71023c142979c4b4c99726
Nehith Mateti <nehith.m@gmail.com>
2022-05-07 15:27:06 +0530

    push

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hi I am Jsai");
-Console.ReadLine();
+string str = Console.ReadLine();
+Console.WriteLine(str);
 Console.WriteLine("Abdul");
 Console.WriteLine("Abdul");
 Console.WriteLine("Abdul");
